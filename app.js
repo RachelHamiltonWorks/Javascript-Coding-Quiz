6db98ff7ca0924d8e1b65b6a08e8d785361f6011
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-04-04 01:33:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,122 @@
+const startButton = document.getElementById("startQuiz")
+
+startButton.addEventListener("click",generateQuiz)
+
+
+function generateQuiz() {
+  console.log("started");
+  var quiz = [
+    {
+      question : "Question 1",
+      answerChoices : [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question : "Question 2",
+      answerChoices : [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question : "Question 3",
+      answerChoices : [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question: "Question 4",
+      answerChoices: [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question: "Question 5",
+      answerChoices: [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question: "Question 6",
+      answerChoices: [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question: "Question 7",
+      answerChoices: [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question: "Question 8",
+      answerChoices: [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question: "Question 9",
+      answerChoices: [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+
+    {
+      question: "Question 10",
+      answerChoices: [
+        "Correct answer",
+        "Wrong answer 1",
+        "Wrong answer 2",
+        "Wrong answer 3"
+      ]
+      
+    },
+  ]
+}
+
+
+
