7df9c289cd4881b14daaaccc8b74dab4c0c022c8
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-04-04 22:45:56 -0400

    added timer

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 //Start quiz on button click "Start Quiz"
-const startButton = document.getElementById("startQuiz")
+const startButton = document.getElementById("startQuizAndTimer")
 startButton.addEventListener("click", generateQuiz)
 
+
 //Variables
-var quizContainer = document.getElementById("quiz");
+var quizContainer = document.getElementById("quizContainer");
 var scoreContainer = document.getElementById("score"); //tallies points, will save to high scores
 var highScores
 
+//Timer 
+document.getElementById("startQuizAndTimer").addEventListener("click", function(){
+  var timeleft = 100;
+
+  var timer = setInterval(function function1(){
+  document.getElementById("countdown").innerHTML = timeleft + 
+  "&nbsp"+"seconds remaining";
+
+  timeleft -= 1;
+  if(timeleft <= 0){
+      clearInterval(timer);
+      document.getElementById("countdown").innerHTML = "Time is up!"
+  }
+  }, 1000);
+
+  console.log(countdown);
+});
+
 //Quiz Generating Fuction aka the Quiz Machine
-//if 'true' add to 10 to scoreContainer, if false subtract 10 from timer
+//loop over all arrays (Q/A sets) with:
+//if correct answer == true add to 10 to scoreContainer, else subtract 10 from timer
 function generateQuiz() {
   console.log("generated");
  quiz = [
