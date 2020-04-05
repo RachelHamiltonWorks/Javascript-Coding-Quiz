6f58405b3fb5ea893d36c32f3da4a28c524f3fdc
Rachel Hamilton <rachelatunccoding@gmail.com>
2020-04-04 21:53:56 -0400

    adding pseudocode and framework

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+//Start quiz on button click "Start Quiz"
 const startButton = document.getElementById("startQuiz")
+startButton.addEventListener("click", generateQuiz)
 
-startButton.addEventListener("click",generateQuiz)
+//Variables
+var quizContainer = document.getElementById("quiz");
+var scoreContainer = document.getElementById("score"); //tallies points, will save to high scores
+var highScores
 
-
+//Quiz Generating Fuction aka the Quiz Machine
+//if 'true' add to 10 to scoreContainer, if false subtract 10 from timer
 function generateQuiz() {
-  console.log("started");
-  var quiz = [
+  console.log("generated");
+ quiz = [
     {
       question : "Question 1",
       answerChoices : [
@@ -25,98 +31,98 @@ function generateQuiz() {
         "Wrong answer 2",
         "Wrong answer 3"
       ]
-      
-    },
+    }
+   ]
+  }
 
-    {
-      question : "Question 3",
-      answerChoices : [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question : "Question 3",
+    //   answerChoices : [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
+    // },
 
-    {
-      question: "Question 4",
-      answerChoices: [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question: "Question 4",
+    //   answerChoices: [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
+    // },
 
-    {
-      question: "Question 5",
-      answerChoices: [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question: "Question 5",
+    //   answerChoices: [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
+    // },
 
-    {
-      question: "Question 6",
-      answerChoices: [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question: "Question 6",
+    //   answerChoices: [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
+    // },
 
-    {
-      question: "Question 7",
-      answerChoices: [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question: "Question 7",
+    //   answerChoices: [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
+    // },
 
-    {
-      question: "Question 8",
-      answerChoices: [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question: "Question 8",
+    //   answerChoices: [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
+    // },
 
-    {
-      question: "Question 9",
-      answerChoices: [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question: "Question 9",
+    //   answerChoices: [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
+    // },
 
-    {
-      question: "Question 10",
-      answerChoices: [
-        "Correct answer",
-        "Wrong answer 1",
-        "Wrong answer 2",
-        "Wrong answer 3"
-      ]
+    // {
+    //   question: "Question 10",
+    //   answerChoices: [
+    //     "Correct answer",
+    //     "Wrong answer 1",
+    //     "Wrong answer 2",
+    //     "Wrong answer 3"
+    //   ]
       
-    },
-  ]
-}
+    // }
+
 
 
 
